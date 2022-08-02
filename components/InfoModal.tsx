91b523942fff0d6e046c96871a2dc4e7dccf0441
Moyowaaaa <moyowa7@icloud.com>
2022-08-02 14:18:09 +0100

    update

--- components/InfoModal.tsx
+++ components/InfoModal.tsx
@@ -1,5 +1,16 @@
 import React from 'react'
 
+
+interface Props {
+  set: boolean;
+  id:number;
+  title:string,
+  rating:number,
+  description:string,
+  date:string | number,
+  image:string 
+}
+
 const InfoModal:React.FC<Props> = ({set,id, title, description, rating, date, image}) => {
   console.log("imag",image)
   return (
@@ -8,7 +19,7 @@ const InfoModal:React.FC<Props> = ({set,id, title, description, rating, date, im
     
         <div className='modal-content'>
             <div className='modal-header'>
-                <p className='w-[3rem] h-[3rem] rounded-full right-0 absolute py-4 mt-2 border-2 flex items-center justify-center cursor-pointer mr-4 ' onClick={() => {
+                <p className='w-[3rem] h-[3rem] rounded-full right-0 absolute py-4 mt-2 flex items-center justify-center cursor-pointer mr-4 bg-[black]' onClick={() => {
                     set(false)
                 }}>X</p>
                 <div className='pl-4 absolute bottom-12'>
@@ -18,21 +29,9 @@ const InfoModal:React.FC<Props> = ({set,id, title, description, rating, date, im
                   </div>
 
 </div>
-                {/* <div className='pl-4'>
-                <p className='z-50 text-[3.3rem]'>{title}</p>
-                <div className='w-full flex flex-col'>
-                  <p className='text-sm w-9/12'>{description}</p>
+              
 
-                  <div className='flex w-7/12 border-2 border-[red] gap-12'>
-                    <small className='text-[#46d369] text-lg'>{rating}/10 Rating </small>
-
-                    <small className='text-[#46d369] text-lg'>{date}</small>
-                    
-                  </div>
-                </div>
-                </div> */}
-
-<div className='flex w-full bg-[black] pl-4  gap-12 absolute bottom-0'>
+<div className='flex w-full bg-[black] pl-4  gap-12 absolute bottom-0 py-2'>
                     <small className='text-[#46d369] text-lg'>{rating}/10 Rating </small>
 
                     <small className='text-[#46d369] text-lg'>{date}</small>
