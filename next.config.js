95e48632654702472a5ded0a6b07ab50d14663ae
Moyowaaaa <moyowa7@icloud.com>
2022-08-04 21:09:25 +0100

    production fixes

--- next.config.js
+++ next.config.js
@@ -1,5 +1,8 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
+api_key:{
+  api_key : process.env.NEXT_PUBLIC_API_KEY
+},
   reactStrictMode: true,
   images: {
     domains: ['image.tmdb.org'],
